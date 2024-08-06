aa3c37913342c38f14b48adb9e2d121f08039e2d
Elvis Cosentino <elviscosentino@hotmail.com>
2024-08-06 11:39:31 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
+clear
+echo
+echo "============================================================"
+echo
+echo "Instalador do sistema de transcrição de áudio."
+echo "Criado por Elvis Cosentino"
+echo
+echo "============================================================"
+echo
+
 sudo apt install python3
 sudo apt install python3-tk
 sudo apt install python3-pip
 pip install ffmpeg-python
 pip install openai-whisper
+
+curl -O https://raw.githubusercontent.com/elviscosentino/transcriber/main/transcriber.py
+chmod +x transcriber.sh
+
+echo
+echo "============================================================"
+echo
+echo "Instalação concluída!"
+echo
+echo "============================================================"
+echo
